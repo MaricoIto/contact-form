eaf38a2386284f9cc6e5f74f20cd86d22e5b73ec
Maricos <itomarico15@gmail.com>
2024-07-28 16:13:22 +0900

    0-6

--- src/app/Http/Controllers/ContactController.php
+++ src/app/Http/Controllers/ContactController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Http\Requests\ContactRequest;
 use App\Models\Contact;
 
 class ContactController extends Controller
@@ -12,17 +12,18 @@ class ContactController extends Controller
         return view('index');
     }
 
-    public function confirm(Request $request)
+    public function confirm(ContactRequest $request)
     {
+        $validated = $request->validated();
         $contact = $request->only(['name', 'tel', 'email', 'content']);
-        // return view('confirm', ['contact' => $contact]);
-        return view('confirm', compact('contact'));
+        return view('confirm', ['contact' => $contact]);
     }
 
-    public function store(Request $request)
+    public function store(ContactRequest $request)
     {
         $contact = $request->only(['name', 'email', 'tel', 'content']);
         Contact::create($contact);
+
         return view('thanks');
     }
 }
